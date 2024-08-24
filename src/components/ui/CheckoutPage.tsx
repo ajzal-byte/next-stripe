70c974b4ac58ceff00f4009b8de2746b3aa93c49
ajzal-byte <ajzalaju10@gmail.com>
2024-08-24 21:41:59 +0530

    link updated

--- src/components/ui/CheckoutPage.tsx
+++ src/components/ui/CheckoutPage.tsx
@@ -50,7 +50,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
         elements,
         clientSecret,
         confirmParams: {
-          return_url: `http://localhost:3000/payment-success?amount=${amount}`,
+          return_url: `https://next-stripe-rho-eight.vercel.app/payment-success?amount=${amount}`,
         },
       });
 
